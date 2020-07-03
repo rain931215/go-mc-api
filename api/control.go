171bd984cb6db0b84eea2c570405cdeaab0e2831
rain931215 <rain931215@gmail.com>
2020-07-03 16:47:41 +0800

    修復控制的function不小心被刪除的問題

--- api/control.go
+++ api/control.go
@@ -5,6 +5,13 @@ import (
 	pk "github.com/Tnze/go-mc/net/packet"
 )
 
+type Hand int32
+
+const (
+	MainHand Hand = iota
+	OffHand
+)
+
 func (c *Client) Chat(msg string) {
 	c.SendPacket(pk.Marshal(data.ChatMessageServerbound, pk.String(msg)))
 }
@@ -59,5 +66,70 @@ func (c *Client) MoveAndRotation(x, y, z float64, yaw, pitch float32, onGround b
 		pk.Float(yaw),
 		pk.Float(pitch),
 		pk.Boolean(onGround),
+	))
+}
+func (c *Client) StartBreakBlock(x, y, z int, direction Direction) {
+	c.SendPacket(pk.Marshal(
+		data.PlayerDigging,
+		pk.VarInt(0),
+		pk.Position{X: x, Y: y, Z: z},
+		pk.Byte(direction),
+	))
+}
+func (c *Client) CancelBreakBlock(x, y, z int, direction Direction) {
+	c.SendPacket(pk.Marshal(
+		data.PlayerDigging,
+		pk.VarInt(1),
+		pk.Position{X: x, Y: y, Z: z},
+		pk.Byte(direction),
+	))
+}
+func (c *Client) FinishBreakBlock(x, y, z int, direction Direction) {
+	c.SendPacket(pk.Marshal(
+		data.PlayerDigging,
+		pk.VarInt(2),
+		pk.Position{X: x, Y: y, Z: z},
+		pk.Byte(direction),
+	))
+}
+func (c *Client) AttackEntity(id int32) {
+	c.SendPacket(pk.Marshal(
+		data.UseEntity,
+		pk.VarInt(id),
+		pk.VarInt(0),
+	))
+}
+func (c *Client) SwitchHotBar(slot int16) {
+	// 接受0~8的格數
+	if slot < 0 || slot > 8 {
+		panic("switch hot bar error: unknown slot")
+	}
+	c.SendPacket(pk.Marshal(
+		data.HeldItemChangeServerbound,
+		pk.Short(slot),
+	))
+}
+func (c *Client) CloseWindow(id uint8) {
+	c.SendPacket(pk.Marshal(
+		data.CloseWindowServerbound,
+		pk.UnsignedByte(id),
+	))
+}
+func (c *Client) PlaceBlock(hand Hand, x, y, z int, face Direction, cursorX, cursorY, cursorZ float32, insideBlock bool) {
+	c.SendPacket(pk.Marshal(
+		data.PlayerBlockPlacement,
+		pk.VarInt(hand),
+		pk.Position{X: x, Y: y, Z: z},
+		pk.VarInt(face),
+		pk.Float(cursorX),
+		pk.Float(cursorY),
+		pk.Float(cursorZ),
+		pk.Boolean(insideBlock),
+	))
+}
+func (c *Client) SwingArm(hand Hand) {
+	c.SendPacket(pk.Marshal(
+		data.AnimationServerbound,
+		pk.VarInt(hand),
 	))
 }
