f680a406014bbf26a38f3bc567756bf798852758
Lirsty <63344950+Lirsty@users.noreply.github.com>
2020-07-24 19:32:14 +0800

    揮手

--- plugin/killAura/killaura.go
+++ plugin/killAura/killaura.go
@@ -77,6 +77,9 @@ func (p *Killaura) onTimeUpdate(age, timeOfDay int64) bool {
 }
 func (p *Killaura) attack() {
 	list := p.getAttackList()
+	if len(list) > 0 {
+		p.c.SwingArm(api.MainHand)
+	}
 	for i := 0; i < len(list); i++ {
 		p.c.AttackEntity(list[i])
 	}
